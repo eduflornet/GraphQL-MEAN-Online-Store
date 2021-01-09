ac60503e423bde3fb2944ffb56d794cb5b7a4a9b
Eduardo Flores <eduflornet@users.noreply.github.com>
2021-01-09 11:34:34 +0100

    Added route empty and not found, module dashboard, module users, component dashboard, component users. Routes configuration and Added dashboard and user routers in app.component.

--- frontend/meang-frontend/src/app/app-routing.module.ts
+++ frontend/meang-frontend/src/app/app-routing.module.ts
@@ -1,29 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {
-    path: `home`, loadChildren: () =>
-      import('./@pages/home/home.module').then(m => m.HomeModule)
+    path: `home`,
+    loadChildren: () =>
+      import('./@pages/home/home.module').then((m) => m.HomeModule),
   },
   {
-    path: `about`, loadChildren: () =>
-      import('./@pages/about/about.module').then(m => m.AboutModule)
+    path: `about`,
+    loadChildren: () =>
+      import('./@pages/about/about.module').then((m) => m.AboutModule),
   },
   {
-    path: `contact`, loadChildren: () =>
-      import('./@pages/contact/contact.module').then(m => m.ContactModule)
+    path: `contact`,
+    loadChildren: () =>
+      import('./@pages/contact/contact.module').then((m) => m.ContactModule),
   },
-  { path: ``, redirectTo: `home`, pathMatch: `full` }
+  {
+    path: `admin`,
+    loadChildren: () =>
+      import('./@admin/pages/dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: `admin/users`,
+    loadChildren: () =>
+      import('./@admin/pages/users/users.module').then((m) => m.UsersModule),
+  },
+  { 
+    path: ``, 
+    redirectTo: `home`, 
+    pathMatch: `full` },
+  {
+    path: `**`,
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,
-    {
+  imports: [
+    RouterModule.forRoot(routes, {
       useHash: true,
-      scrollPositionRestoration: 'enabled'
-    })],
-  exports: [RouterModule]
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
